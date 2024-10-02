bd421b6df9576725ef2fdbdb2244f6c6488ae5fa
jpossandon <jossando@bpn062.psych1.uni-hamburg.de>
2024-10-02 15:48:39 +0200

    extended the instrunctions, not finished yet

--- 05_Code/image_filtering_instructions.m
+++ 05_Code/image_filtering_instructions.m
@@ -2,20 +2,25 @@
 % 1.0.0
 %
 % By José Ossandón (<jose.ossandon@uni-hamburg.de>)
+%
+% To reproduce this script it is necessary to have the filtering function
+% contianed in the following github repository https://github.com/jpossandon/image_filtering.git
+% and https://github.com/jpossandon/csf_psi.git
 %% Image preparation
 %
 % Let's first open an example image and show it. The original image is 400x400
 % pixels but we are going to reshape it to a non-square size for the
 % examples below.
 clear
-imagePath          = '/Users/jossando/trabajo/CSF/KDEF_400_final_cut/400px_600_AF31NEHL.bmp';
+thisScriptDir      = fileparts(mfilename('fullpath'))
+imagePath          = fullfile(thisScriptDir,'400px_600_AF31NEHL.bmp');
 thisImage          = imread(imagePath);
 [m,n,p]            = size(thisImage);
 imshow(thisImage)
 title(sprintf('%dx%d image',m,n))
 %%
-% Rehsaping of the images to 600x650. This makes it bigger but also changes
-% a bit the aspect ratio. In generla, to change the aspect ratio is not
+% Reshaping the image to 600x650. This makes it bigger but also changes
+% a bit the aspect ratio. In general, to change the aspect ratio is not
 % be recommendable, but here we are doing to learn how to deal with
 % non-square images
 targetSizeInPix    = [600 650];      % be careful with the ordering of numbers, MATLAB works with (rows,columns) but screen resolution are usually expressed in width,height    
@@ -29,9 +34,12 @@ title(sprintf('%dx%d image',m,n))
 % # Image background: if the images are not going to be presented to the
 % full size of the screen, it is important to take care which will be the
 % background color, usually set to black or the midlle gray level (level of 0.5 in a scale of 0 to 1 
-% or 127/128 in a scale between 0 and 5). It can also be that the original
+% or 127/128 in a scale between 0 and 255). 
+% 
+% It can also be that the original
 % image has already a background color that needs to be taken in account.
-% Our example image has a gray background (138). 
+% Our example image has a gray background (138/255). 
+%
 % # Image aspect ratio: to use the code presented here images need to be a
 % square. Otherwise the butterworth and CSF filters with not work
 % correctly. This can be achieved in two main ways, by preparing the image
@@ -51,7 +59,7 @@ end
 imshow(square_image)
 title(sprintf('%dx%d image centered in a %dx%d gray background',m,n,bkg_size(1),bkg_size(2)))
 %% 
-% Alternatively, this is how it would look with a black background
+% Alternatively, this is how it would look like with a black background
 bkg_color            = 0;
 square_image_black  = uint8(repmat(bkg_color.*ones(bkg_size(1),bkg_size(2)),1,1,3));
 
@@ -63,8 +71,8 @@ title(sprintf('%dx%d image centered in a %dx%d black background',m,n,bkg_size(1)
 
 %% Image filtering: guassian filter with image_filter.m
 %
-% The function image_filter.m requires two inputs and image to fitler  and a 'options' structure 
-% which defines the different parameters of the function (see documentation).
+% The function image_filter.m requires two inputs, an image to filter and an 'options' structure 
+% which defines the different parameters of the function (see  image_filter.m documentation for details).
 % To filter an image is first necessary to define the image resolution in
 % pixels per degree. This need to be calculated from the image (screen)
 % size, its size in pixels, and the distance of observation. For
@@ -78,10 +86,10 @@ screen_vis_degree       = screen_vis_degree*180/pi; % which can be transformed t
 
 pixels_per_degree       = round(horizontal_resolution./screen_vis_degree); % and the pixells per degree can be caluclated by dividing the screen/image resolution by how many visual degrees it spans (which we round to the next integer) 
 display(sprintf('At a distance of %1.1f cms, %1.2f cms cover %1.2f visual degrees.\n',screen_distance,screen_size_horizontal,screen_vis_degree))
-display(sprintf('At that distance and screen sixe, %d pixels result in a resolution of %d pixels per degreee (ppd).',horizontal_resolution,pixels_per_degree ))
+display(sprintf('At that distance and screen size, %d pixels result in a resolution of %d pixels per degreee (ppd).',horizontal_resolution,pixels_per_degree ))
 
 %%
-% The filter gaussian can be set as options.filterType 'gaussian' or
+% The filter gaussian can be set  options.filterType 'gaussian' or
 % 'gaussian_custom_cutoff'. Let's look first at 'gaussian', for which we
 % only need to define the cutoff frequency:
 
@@ -97,7 +105,7 @@ title(sprintf('%dx%d gray background image, %s filter cutoff %d cyc/deg cutoff',
 % The cutoff for a guassian filter withoout additional specification meens
 % a reduction of ~0.607 in amplitude at that frequency and with a very slow
 % 'roll-off'. To see this we can add the option of computing the image and
-% filtered image (radial) spectra and plotting it
+% filtered image (radial) spatial spectra and plotting it
 
 optionsG3.computeRadialSpectra      = 1;
 optionsG3.toplot                    = 1; %  
@@ -105,7 +113,7 @@ optionsG3.toplot                    = 1; %
 
 %%
 % A reduction of only 0.607 amplitude at a given frequency might be too restricting or little. 
-% Therefore we can only specifiy a desired amplitude reduction at agiven frequency. For this
+% Therefore we can only specifiy a desired amplitude reduction at a given frequency. For this
 % we use the filterType 'gaussian_custom_cutoff'. For example, let's filter with a reduction of 10% amplitude
 % at 3 cyc/degree, and compare the images and spectra to the standard gaussian filter described above:
 
@@ -120,10 +128,10 @@ fh = figure;
 fh.Position(3:4) = fh.Position(3:4)*2; % just making the figure bigger 
 subplot('Position',[.05 .55 .4 .4])
 imshow(lpimageG3_1)
-title(sprintf('%s filter cutoff %d cyc/deg %1.1f cutoff',optionsG3_1.filterType,optionsG3_1.cutoff(1),optionsG3_1.cutoff(2)))
+title(sprintf('%s filter cutoff %d cyc/deg %1.1f amp. red.',optionsG3_1.filterType,optionsG3_1.cutoff(1),optionsG3_1.cutoff(2)))
 subplot('Position',[.55 .55 .4 .4])
 imshow(lpimageG3)
-title(sprintf('%s filter cutoff %d cyc/deg std cutoff',optionsG3.filterType,optionsG3.cutoff(1)))
+title(sprintf('%s filter cutoff %d cyc/deg std 0.607 amp. red.',optionsG3.filterType,optionsG3.cutoff(1)))
 
 % this plots the spectra of the two filters
 subplot('Position',[.05 .05 .4 .4])
@@ -151,46 +159,177 @@ hh(2) = plot(filtersG3_1(size(filtersG3_1,1)/2+1,:));
 legend(hh,{'gauss','gauss_custom_cutoff'})
 title('filters cross-section')
 
-%% Image filtering: butterwoA different type of filter
+%%
+% Note that the filter with a reduction of 0.1 ampitude, not only reduce
+% much more the amplitude at the cut-off frequency but also at lower
+% frequencies, resulting in a much more blurred image.
+
+%% Image filtering: butterworth filter with image_filter.m
 % The second type of filter available in image_filter.m is a butterworth
-% filter. As the guassian filter it can be specified as a standard 'butterworth', in which
-% the cutoff freuqency is where the a,plitude is reduced in half, o as a'butterwoth_custom_cutoff'
+% filter. As with the guassian filter, it can be specified as a standard 'butterworth', in which
+% the cutoff frequency is where the amplitude is reduced 0.71, or as a 'butterwoth_custom_cutoff'
 % in which the cutoff frequency amplitude reduction can be specified.
-% In addition,these filters need their 'order' to be specified. Filters with a lower order (e.g.,1) have
-% a very slow roll-off similar to the gaussian filter. Higer order filters
+% In addition,these filters need that their 'order' to be specified. Filters with a lower order (e.g.,1) have
+% a very slow roll-off, similar to the gaussian filter. Higer order filters
 % have a faster roll-off at the expense of having 'ripples' in the passband
-% and stopband, this translates to 'riginng artifacts' in the spatial
+% and stopband, this translates to 'ringinng artifacts' in the spatial
 % domain. 
 
 % Let's see first a standard 'butterworth', with thre different orders
 % 1,3,6:
-optionsG3_butt.pixxgrade               = pixels_per_degree;
-optionsG3_butt.filterType              = 'butterworth';
-optionsG3_butt.cutoff                  = [3];        % for custom_cutoff the first value is the cutoff requency and the second one the amplitude reduction  
-optionsG3_butt.order                   = 1;
-optionsG3_butt.computeRadialSpectra    = 1;
-optionsG3_butt.toplot                  = 0; %  
-[~,lpimageG3_butt_ord1,filtersG3_butt_ord1,radialAvgFrqG3_butt_ord1] =image_filter(square_image,optionsG3_butt);
-figure,imshow(lpimageG3_butt_ord1)
-optionsG3_butt.order                   = 5;
-[~,lpimageG3_butt_ord5,filtersG3_butt_ord5,radialAvgFrqG3_butt_ord5] =image_filter(square_image,optionsG3_butt);
-figure,imshow(lpimageG3_butt_ord5)
-optionsG3_butt.order                   = 10;
-[~,lpimageG3_butt_ord10,filtersG3_butt_ord10,radialAvgFrqG3_butt_ord10] =image_filter(square_image,optionsG3_butt);
-figure,imshow(lpimageG3_butt_ord10)
+optionsButt3.pixxgrade               = pixels_per_degree;
+optionsButt3.filterType              = 'butterworth';
+optionsButt3.cutoff                  = [3];       
+optionsButt3.order                   = 1;
+optionsButt3.computeRadialSpectra    = 1;
+optionsButt3.toplot                  = 0; %  
+[~,lpimageButt3_ord1,filtersButt3_ord1,radialAvgFrqButt3_ord1] =image_filter(square_image,optionsButt3);
+figure,imshow(lpimageButt3_ord1)
+title(sprintf('%s filter cutoff %d cyc/deg %d order std 0.5 amp. red.',optionsButt3.filterType,optionsButt3.cutoff(1),optionsButt3.order))
+pause(1) % for some reason the cript need to pause a bit otherwise the titles go to the wrong figures
 
+optionsButt3.order                   = 5;
+[~,lpimageButt3_ord5,filtersButt3_ord5,radialAvgFrqButt3_ord5] =image_filter(square_image,optionsButt3);
+figure,imshow(lpimageButt3_ord5)
+title(sprintf('%s filter cutoff %d cyc/deg %d order std 0.5 amp. red.',optionsButt3.filterType,optionsButt3.cutoff(1),optionsButt3.order))
+pause(1)
 
-% The cutoff for a guassian filter withoout additional specification meens
-% a reduction of ~0.607 in amplitude at that frequency and with a very slow
-% 'roll-off'. To see this we can add the option of computing the image and
-% filtered image (radial) spectra and plotting it
-
-% options.padding     = [];
-% options.cutoff      = 3;
-% options.filterType  = 'gaussian';% 'butterworth';%
-% options.toplot      = 1; %   - 1 to toplot filtered images and the filters
-% options.computeRadialSpectra      = 1;
-% [hpimage, lpimageG3, filterG3, radialAvgFrqG3] =image_filter(image,options);
+optionsButt3.order                   = 10;
+[~,lpimageButt3_ord10,filtersButt3_ord10,radialAvgFrqButt3_ord10] =image_filter(square_image,optionsButt3);
+figure,imshow(lpimageButt3_ord10)
+title(sprintf('%s filter cutoff %d cyc/deg %d order std 0.5 amp. red.',optionsButt3.filterType,optionsButt3.cutoff(1),optionsButt3.order))
 
 %%
-% close all
+% As with the gaussian filter, the butterwoth filter can also be specified
+% to have a desired amplitude reduction at a given frequency. For this
+% we use the filterType 'butterworth_custom_cutoff'. Let's compare the
+% filter above order 5 withone with the same order but a reduction of
+% amplitude of 0.1 at the cutoff frequency:
+
+optionsButt3_1.pixxgrade               = pixels_per_degree;
+optionsButt3_1.filterType              = 'butterworth_custom_cutoff';
+optionsButt3_1.cutoff                  = [3 .1];        % for custom_cutoff the first value is the cutoff requency and the second one the amplitude reduction  
+optionsButt3_1.order                   = 5;
+optionsButt3_1.computeRadialSpectra    = 1;
+optionsButt3_1.toplot                  = 0; %  
+
+% plotting the already fitlered image
+fh = figure;
+fh.Position(3:4) = fh.Position(3:4)*2; % just making the figure bigger 
+subplot('Position',[.05 .55 .4 .4])
+imshow(lpimageButt3_ord5)
+title(sprintf('%s filter cutoff %d cyc/deg %d order std 0.5 amp. red.',optionsButt3.filterType,optionsButt3.cutoff(1),optionsButt3.order))
+pause(1)
+
+[~,lpimageButt3_1_ord5,filtersButt3_1_ord5,radialAvgFrqButt3_1_ord5] =image_filter(square_image,optionsButt3_1);
+subplot('Position',[.55 .55 .4 .4])
+imshow(lpimageButt3_1_ord5)
+title(sprintf('%s filter cutoff %d cyc/deg %d order %1.1f amp. red.',optionsButt3_1.filterType,optionsButt3_1.cutoff(1),optionsButt3_1.order,optionsButt3_1.cutoff(2)))
+pause(1)
+
+% this plots the spectra of the two filters
+subplot('Position',[.05 .05 .4 .4])
+freq_spacing=.1;
+xx = 0:1:length(radialAvgFrqButt3_ord5{1})-1;
+h(1) = plot(xx,10*log10(radialAvgFrqButt3_ord5{1}.^2),'k','LineWidth',2);   % the original image spectra
+hold on
+h(2) = plot(xx,10*log10(radialAvgFrqButt3_ord5{1}.^2.*.71.^2),'b:');        % the original image spectra with an amplitude reudction of 0.71
+h(3) = plot(xx,10*log10(radialAvgFrqButt3_ord5{1}.^2.*.1.^2),'r:');         % the original image spectra with an amplitude reudction of 0.1
+% plot(0:1:length(radialProfile)-1,10*log10(radialProfile.^2.*ar.^2));
+h(4) = plot(xx,10*log10(radialAvgFrqButt3_ord5{2}.^2));                     % the image filtered with 'butterworth' filter order 5
+h(5) = plot(xx,10*log10(radialAvgFrqButt3_1_ord5{2}.^2));                   % the image filtered with 'butterworth_custom_cutoff' filter order 5
+axis([0 8/freq_spacing -75 10])
+vline([1.5 3]./freq_spacing)
+legend(h,{'Original','0.7*original','0.1*original','Butt0.7%Ord5','Butt0.1%Ord5'},'box','off')
+set(gca,'XTick',0:1./freq_spacing:length(radialAvgFrqButt3_ord5{1})-1,'XTickLabel',0:1:(length(radialAvgFrqButt3_ord5{1})-1)*freq_spacing)
+xlabel('Spatial Frequency (cyc/deg)')
+ylabel('Power (dB)')
+title('Example filtering Butterwoth cut-off 3 cyc')
+pause(1)
+
+% a cross section of the filters
+subplot('Position',[.55 .05 .4 .4])
+hh(1) = plot(filtersButt3_ord5(size(filtersButt3_ord5,1)/2+1,:));, hold on
+hh(2) = plot(filtersButt3_1_ord5(size(filtersButt3_1_ord5,1)/2+1,:));
+legend(hh,{'Butterworth','Butterworth_custom_cutoff'})
+title('filters cross-section')
+pause(1)
+
+%% CSF filter with im_CSF_filter.m
+% The last filtering option is the experimental CSF filter which
+% 'filters' an image following a Contrast Sensitive Function
+% specification according to the truncated log-parabola model 
+% (for details of this CSF model see Lesmes, L. A. (2010). Journal of
+% Vision, 10(3), 1–21).
+%
+% Let's see how the parametrization works, t requires you to install
+% our CSF implementation you can find in https://github.com/jpossandon/csf_psi.git)
+% The truncated log-parabola model requires 4 parameters: peak frequency
+% (p_f), peak sensitivity (gamma), bandwidth(bw) and truncation (delta)
+
+% 'normal human vision' csf parameters
+CSFparams.p_f    = 4;
+CSFparams.gamma  = 200;
+CSFparams.delta  = 1;
+CSFparams.bw     = 2;
+
+% To calcualte the contrast sensitive function we require additionally a
+% set of spatial frequency and csf.m
+spfreqs                         = 2.^[-2:.25:5];
+[S]             = csf(CSFparams.p_f ,CSFparams.gamma,CSFparams.delta,CSFparams.bw ,spfreqs);
+
+fh = figure;
+fh.Position(3) = fh.Position(3)*2; 
+subplot('Position',[.05 .1 .4 .8])
+plotCSF(spfreqs,S);
+title('CSF ''normal'' vision')
+% and 'lower human vision' csf parameters
+CSFparams.p_f    = 1;
+CSFparams.gamma  = 100;
+CSFparams.delta  = .5;
+CSFparams.bw     = 1;
+
+% To calcualte the contrast sensitive function we require additionally a
+% set of spatial frequency and csf.m
+
+[Slow]             = csf(CSFparams.p_f ,CSFparams.gamma,CSFparams.delta,CSFparams.bw ,spfreqs);
+subplot('Position',[.55 .1 .4 .8])
+plotCSF(spfreqs,Slow);
+title('CSF ''low'' vision')
+pause(2)
+
+%%
+% Now, let's filter the image according to the 'low vision' CSF. For this
+% we need to use the function im_CSF_filter.m and specify the CSF parameters (alrady done above) and an option
+% structure (see im_CSF_filter.m documentation for details)
+% (note that the function only takes in account contrast sensitivties for
+% spatial frequencies above 0.5 cycles per degree)
+optionsCSF.pixdegree        = pixels_per_degree;
+optionsCSF.padding          = [];
+optionsCSF.toplot           = {};
+optionsCSF.BPfilter_spacing = 1;
+optionsCSF.filtertype       = 'cosine';
+[mod_Image] = im_CSF_filter(square_image,CSFparams,optionsCSF);
+title(sprintf('Image filtered according to CSF',optionsG3.filterType,optionsG3.cutoff(1)))
+
+fh = figure;
+fh.Position(3) = fh.Position(3)*2; 
+subplot('Position',[.05 .1 .4 .8])
+imshow(mod_Image)
+% to check the resulting spectra we use the image_filter.m (this will also
+% filter the modified image further with the specifcations in the option
+% structure, but we just do not look at this)
+[~, ~, ~, radialSpectraCSFlowvis]      = image_filter(mod_Image,optionsButt3_1);
+
+subplot('Position',[.55 .1 .4 .8]), hold on
+freq_spacing=.1;
+xx = 0:1:length(radialAvgFrqButt3_ord5{1})-1;
+hh(1) = plot(xx,10*log10(radialAvgFrqButt3_ord5{1}.^2),'k','LineWidth',2);   % the original image spectra
+hh(2) = plot(xx,10*log10(radialSpectraCSFlowvis{1}.^2),'r','LineWidth',2);   % CSF filtereed image spectra
+legend(hh,'Original','CSF modified')
+axis([0 8/freq_spacing -75 10])
+set(gca,'XTick',0:1./freq_spacing:length(radialAvgFrqButt3_ord5{1})-1,'XTickLabel',0:1:(length(radialAvgFrqButt3_ord5{1})-1)*freq_spacing)
+
+
+%%
+close all
